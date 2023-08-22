8638823fc7a3d01672a8f287a422971807782724
heleena-fadi <miss.heleena98@gmail.com>
2023-08-22 21:00:08 +0300

    create readme file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./index.css";
 
-function App() {
+const width = 8;
+const candyColors = ["Red", "Orange", "Yellow", "Green", "Blue", "purple"];
+
+const App = () => {
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
+
+const checkForColumnOfThree = () => {
+  for (let i = 0; i < width; i++) {
+    const columnOfThree = [i, i + width,  i+ width * 2];
+    const decidedColor = currentColorArrangement[i];
+
+    if (columnOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
+      columnOfThree.forEach(square => currentColorArrangement[square] = '')
+    }
+    }
+}
+  const createBoard = () => {
+    const randomColorArrangement = [];
+
+    for (let i = 0; i < width * width; i++) {
+      const randomColor =
+        candyColors[Math.floor(Math.random() * candyColors.length)];
+      randomColorArrangement.push(randomColor);
+    }
+    setCurrentColorArrangement(randomColorArrangement);
+  };
+  useEffect(() => {
+    createBoard();
+  }, []);
+
+  useEffect(() => {
+     checkForColumnOfThree();
+    }, []);
+
+
+  console.log(currentColorArrangement);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="game">
+        {currentColorArrangement.map((candyColors, index) => (
+          <img key={index} style={{ background: candyColors }} alt={candyColors} />
+        ))}
+      </div>
     </div>
   );
-}
-
+};
 export default App;
+
+// stopped in 32 minutes
